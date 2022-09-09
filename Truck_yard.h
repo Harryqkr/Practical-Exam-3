d7f6ffc9820d4cae1394345b0c5c506ba3992d4f
Harryqkr <95751147+Harryqkr@users.noreply.github.com>
2022-09-09 15:18:35 +0930

    Add files via upload

---
+++ Truck_yard.h
@@ -0,0 +1,27 @@
+#ifndef TRUCK_YARD_H
+#define TRUCK_YARD_H
+#include "Truck.h"
+#include <iostream>
+
+using namespace std;
+
+class Truck_yard
+{
+private:
+    int t_capacity;
+    Truck *trucks;
+    int b_code;
+
+public:
+    int index;
+    Truck_yard();
+    Truck_yard(int capacity);
+    int get_total_stock_count();
+    int get_stock_count(int code);
+    Truck *get_current_stock_list();
+    bool add_stock(Truck c);
+    ~Truck_yard();
+
+};
+
+#endif
---
+++ main-1-2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include "Truck_yard.h"
+#include "Truck.h"
+#include<string>
+
+using namespace std;
+
+int main() {
+
+    return 0;
+}
